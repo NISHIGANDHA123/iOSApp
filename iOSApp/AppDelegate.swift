a5bbd43ee2cf1302bb10c19c4b38996b65184c92
Himanshu Lanjewar <himanshu@Himanshus-MacBook-Pro.local>
2019-01-03 13:59:13 +0530

    Initial commit with table view and navigation bar creation with sample data

--- iOSApp/AppDelegate.swift
+++ iOSApp/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  iOSApp
-//
-//  Created by Himanshu Lanjewar on 03/01/19.
-//  Copyright © 2019 iOS APP. All rights reserved.
-//
 
 import UIKit
 
@@ -16,6 +9,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let mainViewController = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: mainViewController)
+        window?.makeKeyAndVisible()
         return true
     }
 
@@ -40,7 +37,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
--- iOSApp/ViewController.swift
+++ iOSApp/ViewController.swift
@@ -1,25 +1,60 @@
-//
-//  ViewController.swift
-//  iOSApp
-//
-//  Created by Himanshu Lanjewar on 03/01/19.
-//  Copyright © 2019 iOS APP. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //variable declaration
+    var detailTableView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //create table view
+        createTableView()
+        
+        //register cell
+        detailTableView.register(UITableViewCell.self, forCellReuseIdentifier:"detailCell")
+        
+        //setting tableview delegate and datasource
+        detailTableView.delegate = self
+        detailTableView.dataSource = self
+        
+        //setup navigation bar
+        setUpNavigation()
+    }
+  
+    //table view creation
+    func createTableView() {
+        detailTableView = UITableView()
+        detailTableView.backgroundColor = .white
+        self.view.addSubview(detailTableView)
+        
+        detailTableView.translatesAutoresizingMaskIntoConstraints = false
+        detailTableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        detailTableView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        detailTableView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true;
+        detailTableView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true;
+    }
+    
+    //Navigation bar creation
+    func setUpNavigation() {
+        navigationItem.title = "About Canada"
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(colorLiteralRed: 0.2431372549, green: 0.7647058824, blue: 0.8392156863, alpha: 1)
+        self.navigationController?.navigationBar.isTranslucent = false
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.init(colorLiteralRed: 1, green: 1, blue: 1, alpha: 1)]
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //table view methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
 
 }
 
